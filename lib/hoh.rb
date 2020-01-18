56776da8955f9ffcd5374d6beac52dc865033551
Wesley Chen <weschen1996@gmail.com>
2020-01-18 04:37:10 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,13 +43,29 @@ def naming_system
   # HoH all the way to Species!
   HoH = {
     PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-  }
+      label: "Kingdom",
+      sub_category: {
+        label: "Phylum",
+        sub_category: {
+          label: "Class"
+        }
+      }
+    }
+    
+    PORTION_2 = {
+      label: "Order"
+    }
+    
+    PORTION_3 = {
+      label: "Family",
+      sub_category: {
+        label: "Genus",
+      }
+    }
+    
+    PORTION_4 = {
+      label: "Species",
+      sub_category: nil
+    }
+}
 end
